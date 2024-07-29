d136a5fcefd0b4e995b212c9c63cc9857cc2de14
admin <admin@DESKTOP-FNI20Q9>
2024-07-29 15:40:28 +0900

    1.GitHub ch01 1차 Up

---
+++ ch01/src/main/java/och02/Board.java
@@ -0,0 +1,52 @@
+package och02;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * Servlet implementation class Board
+ */
+public class Board extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Board() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String title = request.getParameter("title");
+		String writer = request.getParameter("writer");
+		String content = request.getParameter("content");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body><h1>게시판</h1>");
+		out.println("제목 : "+ title +"<p>");
+		out.println("작성자 : "+ writer +"<p>");
+		out.println("작성일 : "+ new Date() + "<p>");
+		out.println("내용"+"<br><pre>"+content+"</pre>");
+		out.close();
+	}
+
+}
